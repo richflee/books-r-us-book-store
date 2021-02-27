39df8d5b662638058dea71113ea77a90d842b89d
Richard Lee <moltesanti@yahoo.com>
2021-02-28 09:40:36 +1100

    Cleaning up styles.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import './App.css';
+import styles from './App.module.css';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 import Header from './components/Header/Header';
 import ProductList from './components/ProductList/ProductList';
-
-const appStyles = {
-  padding: '1em',
-  display: 'flex',
-  margin: '0 auto'
-};
-
-const styles = {
-  flexGrow: '2'
-};
-
-const cartStyles = {
-  flexGrow: '1'
-};
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div style={appStyles}>
-        <div style={styles}>
+      <div className={styles.appContainer}>
+        <div className={styles.productListContainer}>
           <ProductList />
         </div>
-        <div style={cartStyles}>
+        <div className={styles.cartContainer}>
           <ShoppingCart />
         </div>
       </div>
--- src/components/AddToCartButton/AddToCartButton.js
+++ src/components/AddToCartButton/AddToCartButton.js
@@ -4,16 +4,6 @@ import styles from './AddToCartButton.module.css';
 
 const AddToCartButton = function({ name, price }) {
 
-  const style = {
-    border: '1px solid #e85a4f',
-    background: 'transparent',
-    color: '#e85a4f',
-    fontFamily: 'monospace',
-    fontWeight: 'bold',
-    cursor: 'pointer',
-    padding: '0.6em 1em',
-  };
-
   return (
     <div>
       <button className={styles.btn} onClick={() => shoppingCartVar([...shoppingCartVar(), { title: name, price }])}>Add to cart</button>
